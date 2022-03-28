30eb54dd454052e432664ef2de722d485faed770
Jakub Olech <jakub@codeyellow.nl>
2022-03-28 14:14:02 +0200

    Add column settings plugin

---
+++ binder/plugins/column_settings/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django.conf import settings
+
+from ...models import BinderModel
+
+
+class ColumnSetting(BinderModel):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name='+',
+    )
+    view = models.TextField()
+    columns = models.JSONField()
+    class Meta(BinderModel.Meta):
+        unique_together = [('user', 'view')]
